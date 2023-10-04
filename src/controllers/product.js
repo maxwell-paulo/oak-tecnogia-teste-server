8181b62230491e67f2d44658f44b380d90cdaa90
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-03 23:02:43 -0300

    create new product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,1 +1,20 @@
 import handleServerError from "../utils/serverError.js";
+import { createProduct } from "../repositories/productsRepository.js";
+
+const createProductController = async (req, res) => {
+  const { name, description, value, available } = req.body;
+
+  if (!name || !description || !value || !available) {
+    return res.status(500).json({ error: "Must fill in all fields" });
+  }
+
+  try {
+    const newProduct = await createProduct(req.body);
+
+    return res.status(201).json(newProduct);
+  } catch (error) {
+    return handleServerError(error);
+  }
+};
+
+export { createProductController };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { createProductController } from "../controllers/product.js";
 
 export const router = Router();
+
+router.post("/product", createProductController);
